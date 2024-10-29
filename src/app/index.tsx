1cbddbb5765173c2e377aab9a11c275aaca99c81
ArtsiomChults <artemorsha@mail.ru>
2024-10-29 13:28:29 +0300

    init

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import '../../global.css';
+
+export default function Index() {
+  return (
+    <View className='flex-1 bg-cyan-800 items-center justify-center'>
+      <Text className='text-6xl color-black'>Open up App.tsx to start working on your app!</Text>
+      <Text className='text-2xl color-black'>Open up App</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
